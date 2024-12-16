6d69fa399633da7a7c51f9d626126b28cd2385dd
Max Mason-Gransby <maximus66@hotmail.co.uk>
2024-12-16 16:17:47 +0000

    fixed date formats

--- util/date.js
+++ util/date.js
@@ -1,5 +1,5 @@
 export function getFormattedDate(date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }
 
 export function getDateMinusDays(date, days) {
